8a089d628e941eff1cd618450c06f06d065cb762
lelik004 <77204561+lelik004@users.noreply.github.com>
2021-01-27 22:40:09 +0300

    Revert "Выполнены задания к уроку №4"

--- 5 task.py
+++
@@ -1,4 +0,0 @@
-from functools import reduce
-new_list = [el for el in range(100, 1001) if el % 2 == 0]
-
-print(reduce(lambda prev_el, el: prev_el * el, new_list))
--- 7 task.py
+++
@@ -1,9 +0,0 @@
-def fact(n):
-    fact = 1
-    for el in range(1, n + 1):
-        fact *= el
-        yield fact
-
-
-for el in fact(int(input('Введите число n: '))):
-    print(el)
